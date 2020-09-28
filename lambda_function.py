e2488c7db03beb405accb230085ff7b646616879
sakshamsahu007 <59371535+sakshamsahu007@users.noreply.github.com>
2020-09-28 18:06:40 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda1234567!')
+        'body': json.dumps('Hello from Lambda. RKS')
     }
